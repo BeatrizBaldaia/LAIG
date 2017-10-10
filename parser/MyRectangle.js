3ca588fcc81dea51fba61c754c4c38c9e1ceab5c
BeatrizBaldaia <bibibaldaia@hotmail.com>
2017-10-10 13:13:54 +0100

    amplif_factor

--- parser/MyRectangle.js
+++ parser/MyRectangle.js
@@ -50,3 +50,16 @@ MyRectangle.prototype.initBuffers = function () {
 	this.primitiveType=this.scene.gl.TRIANGLES;
 	this.initGLBuffers();
 };
+
+MyRectangle.prototype.setAmplifFactor = function (afS, afT) {
+	var size = this.texCoords.length;
+	var i = 0;
+	while(i < 0) {
+		if(i % 2 == 0) {
+			this.texCoords[i] /= afS;
+		} else {
+            this.texCoords[i] /= afT;
+		}
+		i++;
+	}
+};
