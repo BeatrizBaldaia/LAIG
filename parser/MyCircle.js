8d9dca36a5189e8acc79414d4e62c1681260b71e
BeatrizBaldaia <bibibaldaia@hotmail.com>
2017-10-16 22:45:04 +0100

    texturas das tampas corrigida

--- parser/MyCircle.js
+++ parser/MyCircle.js
@@ -9,9 +9,7 @@ MyCircle.prototype = Object.create(CGFobject.prototype);
 MyCircle.prototype.constructor=MyCircle;
 
 MyCircle.prototype.initBuffers = function() {
-
-		var lados = this.slices;
-		var divisions = 2*Math.PI/lados;
+		var divisions = 2*Math.PI/this.slices;
 
 		this.vertices = [];
 		this.indices = [];
@@ -21,19 +19,19 @@ MyCircle.prototype.initBuffers = function() {
 		this.vertices.push(0,0,0);
 		this.normals.push(0,0,1);
 		this.texCoords.push(.5,.5);
-		for(var i = 0; i <= lados; i++){
+		for(var i = 0; i <= this.slices; i++){
 			//vertices
 			this.vertices.push(Math.cos(i*divisions), Math.sin(i*divisions),0);
 			//normais
 			this.normals.push(0,0,1);
 			//TextCoords
-			this.texCoords.push(.5*Math.cos(i*divisions),.5*Math.sin(i*divisions));
+			this.texCoords.push(0.5 + 0.5 * Math.cos(i*divisions),0.5 + 0.5 * Math.sin(i*divisions));
 
 		}
-		for(var i = 0; i < lados; i++){
+		for(var i = 0; i < this.slices; i++){
 			this.indices.push(0,i,i+1);
 		}
-		this.indices.push(0,lados,1);
+		this.indices.push(0,this.slices,1);
 
 		this.primitiveType=this.scene.gl.TRIANGLES;
 		this.initGLBuffers();
