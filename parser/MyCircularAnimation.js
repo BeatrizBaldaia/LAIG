e5d7ee17118aa0263c43c21711cc0709e2e7ae9b
AnabelaSilva <up201506034@fe.up.pt>
2017-11-08 16:31:31 +0000

    Adicionei Circular animation

--- parser/MyCircularAnimation.js
+++ parser/MyCircularAnimation.js
@@ -9,9 +9,9 @@ function MyCircularAnimation(graph, linearVelocity, x,y,z, radius, angleInicial,
 	
 	this.center = [x,y,z];
 	this.radius = radius;
-	this.angleInicial = angleInicial;
-	this.angleRotate = angleRotate;
-	this.linearVelocity = linearVelocity*1000;
+	this.angleInicial = angleInicial * DEGREE_TO_RAD;
+	this.angleRotate = angleRotate * DEGREE_TO_RAD;
+	this.linearVelocity = linearVelocity*0.001;
 	this.angularVelocity = this.linearVelocity/this.radius;
 
 };
@@ -20,12 +20,19 @@ MyCircularAnimation.prototype= Object.create(MyAnimation.prototype);
 MyCircularAnimation.prototype.constructor = MyCircularAnimation;
 
 MyCircularAnimation.prototype.getMatrix = function(currTime) {
-	var aux = mat4.create();
+	if(this.initialTime == undefined){
+		this.initialTime = currTime;
+	}
+	let deltaT = currTime - this.initialTime;
+	let alfa = this.angleInicial + deltaT * this.angularVelocity;
+	if(alfa > (this.angleInicial+this.angleRotate)) { alfa = (this.angleInicial+this.angleRotate);}
+
+	let aux = mat4.create();
     mat4.identity(aux);
-//http://glmatrix.net/docs/module-mat4.html
-    //mat4.translate(aux,[]);
-	//TODO put center in 0 0 0
-	//TODO rotate
-	//TODO put center in place
+    aux = mat4.translate(aux, aux, this.center);
+    aux = mat4.rotate(aux, aux, alfa, [0,1,0]);
+    aux = mat4.translate(aux, aux, [this.radius,0,0]);
+    aux = mat4.rotate(aux, aux, Math.PI/2, [0,1,0]);
+
 	return aux;
 }
