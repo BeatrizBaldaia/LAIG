79aa0a35059a1829f398b4b3458da5b743453e5a
BeatrizBaldaia <bibibaldaia@hotmail.com>
2017-11-19 15:16:49 +0000

    tinha apagado uma chaveta a mais sem querer XD

--- parser/MyLinearAnimation.js
+++ parser/MyLinearAnimation.js
@@ -60,7 +60,6 @@ MyLinearAnimation.prototype.calculateAng = function(point1, point2) {
     return currAng;
 }
 
-
 MyLinearAnimation.prototype.getDeltaTime = function(temp, state) {
     var delta = temp;
     for(let i = 0; i < (state - 1); i++) {
@@ -68,6 +67,7 @@ MyLinearAnimation.prototype.getDeltaTime = function(temp, state) {
     }
     return delta;
 
+}
 
 MyLinearAnimation.prototype.getMatrix = function(initialTime, currTime) {
     var temp = currTime - initialTime;
