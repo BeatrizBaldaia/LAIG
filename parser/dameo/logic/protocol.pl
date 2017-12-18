e84d62ed9c84406ea383c8c108a042f8d6461c22
AnabelaSilva <up201506034@fe.up.pt>
2017-12-18 09:48:06 +0000

    King Moves Done

--- parser/dameo/logic/protocol.pl
+++ parser/dameo/logic/protocol.pl
@@ -12,7 +12,7 @@ jogadaValidaSimplesTeste(CurrBoard, Player, Move):-
 	nth1(1, LInverted, NCaptures-BestMove),
 	getBestCaptures(LInverted, Best, [], NCaptures),
 	ite(NCaptures = 0,
-		simpleMoveTeste(CurrBoard, Player, Move),
+		moveTeste(CurrBoard, Player, Move),
 		fail
 	).
 
@@ -29,12 +29,15 @@ jogadaValidaCapturaTeste(CurrBoard, Player, Move, More):-
 			(More = yes, turnToOneCapture(Best, NewBest), member(Move, NewBest))))
 	).
 
-simpleMoveTeste(CurrBoard, Player, [X-Y,NewX-NewY]):-
+moveTeste(CurrBoard, Player, [X-Y,NewX-NewY]):-
 	ite(
 		Player = 1,
 		validMan1Move(CurrBoard, X-Y, NewX-NewY, D),
 		validMan2Move(CurrBoard, X-Y, NewX-NewY, D)),
 	updateBoardSimpleMove(CurrBoard, NewBoard, X-Y, NewX-NewY, Player).
+moveTeste(CurrBoard, Player, [X-Y,NewX-NewY]):-
+    ite(Player = 1, King = 11, King = 22),
+    validKeepMoving(CurrBoard, X-Y, NewX-NewY, King, D).
 
 turnToOneCapture([], []).
 turnToOneCapture([[A,B|_]|Continue], [[A,B]|NewContinue]):-
